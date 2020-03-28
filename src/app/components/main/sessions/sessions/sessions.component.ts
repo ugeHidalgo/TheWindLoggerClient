4a0af268af32d0511a272d9d31d1091c60f425a0
Eugenio Hidalgo Hernández <eugenio.hidalgo@unit4.com>
2020-03-28 16:45:56 +0100

    Added sesions filter to sessions screen

--- src/app/components/main/sessions/sessions/sessions.component.ts
+++ src/app/components/main/sessions/sessions/sessions.component.ts
@@ -1,17 +1,21 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { Session } from 'src/app/models/session';
 import { MatTableDataSource, MatPaginator, MatSort, MatDialog } from '@angular/material';
 import { SessionsService } from 'src/app/services/sessions/sessions.service';
 import { ToastrService } from 'ngx-toastr';
 import { GlobalsService } from 'src/app/globals/globals.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { SessionFilterData } from 'src/app/models/sessionFilterData';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-sessions',
   templateUrl: './sessions.component.html',
   styleUrls: ['./sessions.component.scss']
 })
-export class SessionsComponent {
+export class SessionsComponent implements OnInit {
 
   selectedRowId: string = '-1';
   userName: string;
@@ -20,6 +24,8 @@ export class SessionsComponent {
   displayedColumns: string[];
   displayedFooterColumns: string[];
   dataSource: MatTableDataSource<Session>;
+  validatingForm: FormGroup;
+  sessionFilterData: SessionFilterData;
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
@@ -35,10 +41,25 @@ export class SessionsComponent {
 
     me.displayedColumns = ['sessionDate', 'name', 'sportName', 'spotName', 'sessionTime', 'sessionDistance','maxSpeed', 'medSpeed'];
     me.userName = me.globals.userNameLogged;
-    me.getSessionsForCompany(me.userName);
+    me.sessionFilterData = new SessionFilterData();
+
+    me.createForm();
+    //me.getSessionsForCompany(me.userName);
   }
 
-  private getSessionsForCompany(userName: string): void {
+  ngOnInit() {
+    const me = this;
+          
+    me.sessionFilterData.userName = me.globals.userNameLogged;
+    me.setSearchPeriod('week');
+
+    me.writeDateFromInForm();
+    me.writeDateToInForm();
+
+    me.onClickSearchButton();
+  }
+
+  /* private getSessionsForCompany(userName: string): void {
     const me = this;
 
     me.globals.maskScreen();
@@ -54,12 +75,13 @@ export class SessionsComponent {
         me.globals.unMaskScreen();
         me.toastr.error(error.message);
       });
-  }
+  } */
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  //Screen events
   onClickAddButton() {
     this.toastr.warning('To be implemented.');
     /* const pathToAccountDetail = `/accountdetail/-1`;
@@ -75,6 +97,87 @@ export class SessionsComponent {
 
   onClickRemoveButton() {
     this.toastr.warning('To be implemented.');
+  }
+
+  onSearchPeriodTypeChange(periodSearchType: string) {
+    const me = this;
+
+    if (periodSearchType === 'year' ||
+        periodSearchType === 'month' ||
+        periodSearchType === 'week' ) {
+          me.setSearchPeriod(periodSearchType);
+
+          me.writeDateFromInForm();
+          me.writeDateToInForm();
+        }
+  }
+
+  onClickSearchButton() {
+    var me = this;
+
+    me.globals.maskScreen();
+
+    me.sessionFilterData.dateFrom = me.getDateFrom();
+    me.sessionFilterData.dateTo = me.getDateTo();
+
+    me.sessionsService.getFilteredSessions(me.sessionFilterData)
+      .subscribe(sessions => {
+        me.sessions = sessions;
+        me.dataSource = new MatTableDataSource<Session>(sessions);
+        me.dataSource.paginator = me.paginator;
+        me.dataSource.sort = me.sort;
+        me.globals.unMaskScreen();
+      },
+      error => {
+        me.globals.unMaskScreen();
+        me.toastr.error(error.message);
+      });
+  }
+
+  // FormModel methods
+  createForm() {
+    const me = this;
+
+    me.validatingForm = new FormGroup({
+      dateFrom: new FormControl('', [Validators.required]),
+      dateTo: new FormControl('', [Validators.required])
+    },
+    { updateOn: 'blur'});
+  }
+
+  getDateFrom(): string {
+    const me = this,
+          formModel = me.validatingForm.value;
+
+    return formModel.dateFrom;
+  }
+
+  writeDateFromInForm() {
+    const me = this;
+
+    me.validatingForm.patchValue({dateFrom: me.sessionFilterData.dateFrom })
+  }
+
+  getDateTo(): string {
+    const me = this,
+          formModel = me.validatingForm.value;
+
+    return formModel.dateTo;
+  }
+
+  writeDateToInForm() {
+    const me = this;
+
+    me.validatingForm.patchValue({dateTo: me.sessionFilterData.dateTo })
+  }
+
+  setSearchPeriod(unitOfTime: moment.unitOfTime.StartOf) {
+    const me = this,
+          startDate = moment().startOf(unitOfTime).format('YYYY-MM-DD[T00:00:00.000Z]'),
+          endDate = moment().endOf(unitOfTime).format('YYYY-MM-DD[T00:00:00.000Z]');
+
+    me.sessionFilterData.dateFrom = startDate;
+    me.sessionFilterData.dateTo = endDate;
   }
 
   selectRow(row) {
